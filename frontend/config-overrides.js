0d8249160c07e42572c90dd65b08aa4be1368a63
rmaskey1 <maskey.reshaj@gmail.com>
2025-04-01 06:25:34 -0700

    overriding configs

---
+++ frontend/config-overrides.js
@@ -0,0 +1,12 @@
+
+
+module.exports = function override(config) {
+    const fallback = config.resolve.fallback || {};
+
+    Object.assign(fallback,{
+        "path": require.resolve("path-browserify")
+    });
+    config.resolve.fallback = fallback;
+
+    return config;
+}
--- frontend/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // other config like entry, output, etc.
-    resolve: {
-      fallback: {
-        path: false, // 👈 disables Node's 'path' module in the browser
-      },
-    },
-  };
-  
