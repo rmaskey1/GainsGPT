f1bb048d2421836d53696cae14aaf73f2eb47329
rmaskey1 <maskey.reshaj@gmail.com>
2025-04-01 06:27:58 -0700

    more config

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -4,7 +4,8 @@ module.exports = function override(config) {
     const fallback = config.resolve.fallback || {};
 
     Object.assign(fallback,{
-        "path": require.resolve("path-browserify")
+        "path": require.resolve("path-browserify"),
+        "os": require.resolve("os-browserify/browser")
     });
     config.resolve.fallback = fallback;
 
